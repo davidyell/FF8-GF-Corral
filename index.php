fb07ff248a0939905796b7de5f6a32bea596d4c6
David Yell <dyell@ukwebmedia.com>
2014-04-22 16:24:11 +0100

    Updated the main bootstrap and displayed the table to start

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Bootstrap the application
+ * 
+ * @author David Yell <neon1024@gmail.com>
+ */
+
+/**
+ * Composer auto-loader
+ */
+require 'vendor/autoload.php';
+
+/**
+ * Bootstrap
+ */
+require 'src/neon1024/Bootstrap.php';
+$site = new \neon1024\Bootstrap();
+$site->render($_SERVER['REQUEST_URI']);
