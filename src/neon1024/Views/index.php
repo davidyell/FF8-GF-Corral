da4781331e77b246b2db67326c650f01e8b10845
David Yell <neon1024@gmail.com>
2014-04-23 18:32:23 +0100

    Updated the pathing to match the new web/public folder

--- src/neon1024/Views/index.php
+++ src/neon1024/Views/index.php
@@ -4,9 +4,9 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Final Fantasy VIII - Guardian Force Corral</title>
 		
-		<link rel="stylesheet" href="/vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
-		<link rel="stylesheet" href="/vendor/twbs/bootstrap/dist/css/bootstrap-theme.min.css">
-		<link rel="stylesheet" href="../../../web/public/assets/css/style.css">
+		<link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/bootstrap/css/bootstrap-theme.min.css">
+		<link rel="stylesheet" href="/assets/css/style.css">
 		
     </head>
     <body>
@@ -32,7 +32,7 @@
 						<?php foreach ($this->viewVars['gfs']->getCollection() as $gf): ?>
 							<tr>
 								<td class='table-label'>
-									<img src='/src/neon1024/Assets/images/<?php echo $gf->getName();?>.png'>
+									<img src='/assets/images/<?php echo $gf->getName();?>.png'>
 									<?php echo $gf->getName(); ?>
 
 									<select name='character' id='<?php echo strtolower($gf->getName());?>' class='select-character'><option>Select character</option>
@@ -84,7 +84,7 @@
 		</div>
 		
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-		<script src="/vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
-		<script src="/src/neon1024/Assets/js/common.js"></script>
+		<script src="/bootstrap/js/bootstrap.min.js"></script>
+		<script src="/assets/js/common.js"></script>
     </body>
 </html>
