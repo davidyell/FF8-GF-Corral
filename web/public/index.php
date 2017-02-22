e9101353195c7f683e853dcc00f399820e4f6f52
David Yell <dyell@ukwebmedia.com>
2017-02-22 16:32:11 +0000

    Tidied up the bootstrap and removed the old require to use the autoloader instead

--- web/public/index.php
+++ web/public/index.php
@@ -4,15 +4,9 @@
  * 
  * @author David Yell <neon1024@gmail.com>
  */
+use neon1024\Bootstrap;
 
-/**
- * Composer auto-loader
- */
-require '../../vendor/autoload.php';
+require dirname(dirname(__DIR__)) . '/vendor/autoload.php';
 
-/**
- * Bootstrap
- */
-require '../../src/neon1024/Bootstrap.php';
-$site = new \neon1024\Bootstrap();
+$site = new Bootstrap();
 $site->dispatch($_SERVER['REQUEST_URI']);
